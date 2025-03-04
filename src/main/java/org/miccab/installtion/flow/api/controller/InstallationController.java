86fdb8d934599ae1a589668c59a05f38bbc8441c
Michal Cabak <miccab@gmail.com>
2025-03-04 19:33:19 +0100

    use singleton

--- src/main/java/org/miccab/installtion/flow/api/controller/InstallationController.java
+++ src/main/java/org/miccab/installtion/flow/api/controller/InstallationController.java
@@ -2,8 +2,10 @@ package org.miccab.installtion.flow.api.controller;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import jakarta.inject.Singleton;
 
 @Controller("/installations")
+@Singleton
 public class InstallationController {
     @Get
     public String list() {
