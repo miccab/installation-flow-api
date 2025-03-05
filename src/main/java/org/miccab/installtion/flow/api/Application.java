b0ff87bf12749cd394a9bb7ef512b6a65b48e8ed
Michal Cabak <miccab@gmail.com>
2025-03-05 08:24:58 +0100

    play with logging

--- src/main/java/org/miccab/installtion/flow/api/Application.java
+++ src/main/java/org/miccab/installtion/flow/api/Application.java
@@ -1,8 +1,15 @@
 package org.miccab.installtion.flow.api;
 
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
 
 public class Application {
+    private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(Application.class);
+
+    public Application() {
+        LOG.info("Starting the application");
+    }
+
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
     }
---
+++ src/main/java/org/miccab/installtion/flow/api/Spike.java
@@ -0,0 +1,12 @@
+package org.miccab.installtion.flow.api;
+
+import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+
+@Singleton
+public class Spike {
+    private static final Logger LOG = org.slf4j.LoggerFactory.getLogger(Spike.class);
+    public Spike() {
+        LOG.info("Spike is here");
+    }
+}
