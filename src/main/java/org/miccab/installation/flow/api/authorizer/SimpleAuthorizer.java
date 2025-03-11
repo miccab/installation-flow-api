5c361258587b46d4398f32bfe7bc27418a1c4675
Michal Cabak <miccab@gmail.com>
2025-03-11 22:18:55 +0100

    add custom authorizer

---
+++ src/main/java/org/miccab/installation/flow/api/authorizer/SimpleAuthorizer.java
@@ -0,0 +1,39 @@
+package org.miccab.installation.flow.api.authorizer;
+
+import io.micronaut.function.aws.MicronautRequestHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class SimpleAuthorizer extends MicronautRequestHandler<Map<String, Object>, Map<String, Object>> {
+    @Override
+    public Map<String, Object> execute(Map<String, Object> input) {
+        boolean authorized = isAuthorized(input);
+        String principalId = "user|a1b2c3d4"; // The principal user identifier
+        String methodArn = (String) input.get("methodArn");
+
+        Map<String, Object> authResponse = new HashMap<>();
+        authResponse.put("principalId", principalId);
+
+        Map<String, Object> policyDocument = new HashMap<>();
+        policyDocument.put("Version", "2012-10-17");
+
+        Map<String, String> statementOne = new HashMap<>();
+        statementOne.put("Action", "execute-api:Invoke");
+        statementOne.put("Effect", authorized ? "Allow" : "Deny");
+        statementOne.put("Resource", methodArn);
+
+        policyDocument.put("Statement", new Map[]{statementOne});
+        authResponse.put("policyDocument", policyDocument);
+        return authResponse;
+    }
+
+    private boolean isAuthorized(Map<String, Object> input) {
+        Map<String, String> headers = (Map<String, String>) input.get("headers");
+        String authorizationHeader = Optional.ofNullable(headers)
+                .map(h -> h.get("Authorization"))
+                .orElse(null);
+        return "my_secret".equals(authorizationHeader);
+    }
+}
